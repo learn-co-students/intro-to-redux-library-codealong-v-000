7a500564e142aa38bb5a859213e2c22ed17bbca5
Idayat <github email address>
2017-05-20 14:56:40 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,12 +1,16 @@
 export default function shoppingListItemReducer(state = {items: []}, action) {
+
   console.log(action)
+
   switch(action.type) {
-    case 'GET_COUNT_OF_ITEMS':
+
+    case 'INCREASE_COUNT':
     let items;
     items = state.items.concat(state.items.length + 1)
     let proposedstate = Object.assign({}, {items: items})
     console.log(proposedstate)
      return proposedstate;
+
     default:
       console.log(state)
       return state;
