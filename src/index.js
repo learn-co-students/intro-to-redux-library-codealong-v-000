a21a55d7a08af8be185c41ed49a1d37c113becc8
Callie Brazil <cmabrazil@gmail.com>
2017-07-19 22:09:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App store={store}/>
+  </Provider>
   document.getElementById('root')
 );
