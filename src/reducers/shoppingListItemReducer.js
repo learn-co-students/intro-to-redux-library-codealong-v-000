c8565d1badf2ed0d6c524ce882c8bff364a8d1b2
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-11-04 14:53:00 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,9 +4,12 @@ export default function shoppingListItemReducer(state = {
   switch(action.type) {
 
     case 'INCREASE_COUNT':
+      console.log("current state.items lenght %s", state.items.length);
+      console.log("updating state.items length to %s", state.items.length + 1);
       return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
 
     default:
+      console.log("initial state.items length: %s", state.items.length);
       return state;
   }
 }
