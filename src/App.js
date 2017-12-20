bb3acac24011547377322d409f38ff66b946e2fd
LS-C <suzy.cho111@gmail.com>
2017-12-20 10:24:18 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  handleClick = () => {
+    this.props.store.dispatch({
+      type: 'INCREASE_COUNT'
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <button onClick={this.handleClick}>BUTTON</button>
         App component
+        <p>{this.props.store.getState().items.length}</p>
       </div>
     );
   }
