69591a981443c595abc6513f17eee1d429f696ac
¨anniezh12¨ <aniqaelahi@yahoo.com>
2018-03-06 22:24:15 -0800

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -2,11 +2,14 @@ export default function shoppingListItemReducer(state = {
   items: []
 }, action) {
   switch(action.type) {
-
+  console.log(action);
     case 'INCREASE_COUNT':
       return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
+      console.log("Current state.items length", state.items.length);
+        console.log("updating state.items length", state.items.length+1);
 
     default:
+      console.log("Initial state.items length", state.items.length);
       return state;
   }
 }
