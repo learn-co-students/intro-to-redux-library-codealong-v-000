3b5bd93c148e9ad5b2c2c9fe33ad1d779dedb48b
mrfarmer7771 <mfarmer@d120.org>
2018-08-12 19:01:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer);
+
+//Create a new store using shoppingLIstItemReducer as the 'reducer' function for handling actions
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
+  //Provider component, from react-redux, allows us to avoid passing 'store', as a prop, to App
+  //This is the only place where store needs to be passed into the App.
   <Provider store={store}>
     <App />
   </Provider>, 
