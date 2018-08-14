2f2258550c0fe4315ac47f78bd8c320aef550f36
seanoughton <seanoughton@gmail.com>
2018-08-13 17:08:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer);
+//const store = createStore(shoppingListItemReducer);//this creates an instance of a store with methods and a state instance variable, it allows us to update the date for this store instance
+
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
 );
