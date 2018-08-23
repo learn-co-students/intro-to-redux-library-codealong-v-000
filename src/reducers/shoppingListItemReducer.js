7cd1abd463df2f8b906245b6ed931a5d207426b3
github username <github email address>
2018-08-23 15:59:34 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,19 @@
 export default function shoppingListItemReducer(state = {
   items: []
 }, action) {
+// Log the action //
   console.log(action);
   switch(action.type) {
 
     case 'INCREASE_COUNT':
       console.log("Current state.items length %s", state.items.length);
       console.log("Updating state.items length to %s", state.items.length + 1);
+
+// If the case statement is met...
+//store the state as the current state first 'Object.assign({}, state, { items: state.items.concat('Then log the updated state value here 'state.items.length + 1' within the parenthesis)'
       return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
 
+//  Last log the previous state under the default case statement if the state is unchanged.
     default:
       console.log("Initial state.items length: %s", state.items.length);
       return state;
