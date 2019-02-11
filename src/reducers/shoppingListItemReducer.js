ce6a876c719865258c67f2e48d049dfae9a404ed
github username <github email address>
2019-02-11 12:43:01 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,11 +4,19 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+		console.log("current state.items ength %s", state.items.length);
+		console.log("updating state.items length to %s", state.items.length + 1);
+
+			return Object.assgin({}, state, {
+				items:
+				state.items.concat(state.items.length + 1)});
 
 		default:
+		console.log('Initial state.items length: %s', state.items.length);
+
 			return state;
 	}
 }
