81438501c69a390a0b29e3d215a4465787e4605e
tracdnguyen <nguyendtrac@gmail.com>
2019-02-22 16:00:57 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -23,13 +23,8 @@ const mapStateToProps = state => {
 	};
 };
 
-const mapDispatchToProps = dispatch => {
-	return {
-		increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
-	};
-};
 
 export default connect(
 	mapStateToProps,
-	mapDispatchToProps
+
 )(App);
