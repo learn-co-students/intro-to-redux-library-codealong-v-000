40c7faaf0e017f01292eca9c4e80a603e20cb8ad
terryblue99 <terryblue@comcast.net>
2019-03-23 16:57:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // ./src/index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux' /* code change */
+
+import shoppingListItemReducer from './reducers/shoppingListItemReducer'
+import App from './App'
+import './index.css'
+
+/* pass through the reducer to the createStore method */
+const store = createStore(
+	shoppingListItemReducer,
+	/* access the browser to find the redux-devtool extension and */
+	/* execute it so that the application can communicate with it */
+	/* https://github.com/zalmoxisus/redux-devtools-extension */
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+) /* code change */
+
+ReactDOM.render(<App store={store} /* code change */ />,
+	document.getElementById('root'))	
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,21 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+    state = {
+        items: []
+    },
+    action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
-
-		default:
-			return state;
-	}
+    console.log(action);
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            console.log('Current state.items length', state.items.length);
+            console.log('Updating state.items length to', state.items.length + 1);
+            return Object.assign({}, state, {
+            	// add items length value to the array
+                items: state.items.concat(state.items.length + 1)
+            });
+ 
+        default:
+            console.log('Initial state.items length:', state.items.length);
+            return state;
+    }
 }
