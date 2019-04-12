9b6442a599052c5c3aad4a0329961fa0805c3e0a
Julian Johannesen <julian.johannesen@gmail.com>
2019-04-12 13:51:15 -0400

    done

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,13 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+export default function shoppingListItemReducer(state={items:[]}, action) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length: ', state.items.length);
+			console.log('Updating state.items length to: ', state.items.length + 1);
+			// Concatenate the previous state.items and a number equal to the number of items in state.items plus 1. That adds a number to the end of the items array.
 			return state.items.concat(state.items.length + 1);
-
 		default:
+			console.log('Initial state.items length: ', state.items.length);
 			return state;
 	}
 }
