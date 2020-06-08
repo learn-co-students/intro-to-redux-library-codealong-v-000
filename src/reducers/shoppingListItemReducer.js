da8eae3dbcd92d7732c89e51507a6288e6ee169c
leothebeagle <a.kosber@gmail.com>
2020-06-08 00:09:10 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,11 +1,9 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+export default function shoppingListItemReducer(state = {items: []},action) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length);
+			console.log('Updating state.items length to %s', state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
@@ -14,3 +12,6 @@ export default function shoppingListItemReducer(
 			return state;
 	}
 }
+
+// the reducer returns an object that represents what the updated state should be
+// dispatch is what actually sets the state in the store to this return value.
