62f93264b19ec21fac5eecf5fe2e5e713da8a15c
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-24 17:42:06 -0500

    Done

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,18 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+  state = {
+    items: [],
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
-	}
+  console.log(action);
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        ...state,
+        items: state.items.concat(state.items.length + 1),
+      };
+
+    default:
+      return state;
+  }
 }
